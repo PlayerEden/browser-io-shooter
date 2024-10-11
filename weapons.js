ddd5da485a863eb474915b7fcfcb04dda965394d
PlayerEden <159679438+PlayerEden@users.noreply.github.com>
2024-10-11 13:19:22 -0400

    Update weapons.js

--- weapons.js
+++ weapons.js
@@ -3,27 +3,31 @@
 let bullets = [];
 const bulletSize = 5;
 const bulletSpeed = 5;
-let weaponType = 'standard';
 
+// Shoot a bullet
 function shootBullet() {
-    if (weaponType === 'standard') {
-        bullets.push({
-            x: playerX + playerSize / 2 - bulletSize / 2,
-            y: playerY,
-            speed: bulletSpeed
-        });
-    } else if (weaponType === 'spread') {
-        bullets.push({ x: playerX + playerSize / 2 - bulletSize / 2, y: playerY, speed: bulletSpeed });
-        bullets.push({ x: playerX + playerSize / 2 - bulletSize / 2 - 10, y: playerY, speed: bulletSpeed });
-        bullets.push({ x: playerX + playerSize / 2 - bulletSize / 2 + 10, y: playerY, speed: bulletSpeed });
-    }
+    bullets.push({
+        x: playerX + playerSize / 2 - bulletSize / 2,
+        y: playerY,
+        speed: bulletSpeed
+    });
 }
 
+// Draw bullets on the canvas
 function drawBullets() {
-    ctx.fillStyle = '#FF0000';
+    ctx.fillStyle = '#FF0000'; // Red bullets
     bullets.forEach(bullet => {
         bullet.y -= bullet.speed;
         ctx.fillRect(bullet.x, bullet.y, bulletSize, bulletSize);
     });
-    bullets = bullets.filter(bullet => bullet.y > 0); // Remove bullets off-screen
+
+    // Remove bullets that move off-screen
+    bullets = bullets.filter(bullet => bullet.y > 0);
 }
+
+// Detect spacebar press to shoot
+window.addEventListener('keydown', (e) => {
+    if (e.key === ' ') {
+        shootBullet();
+    }
+});

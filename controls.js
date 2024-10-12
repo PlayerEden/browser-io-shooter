e9ad96ff980449165d2da87f9fa9905240c724aa
PlayerEden <159679438+PlayerEden@users.noreply.github.com>
2024-10-11 23:58:58 -0400

    Update controls.js

--- controls.js
+++ controls.js
@@ -5,6 +5,11 @@ let keys = {};
 
 window.addEventListener('keydown', (e) => {
     keys[e.key] = true;
+
+    // Shoot when the space bar is pressed
+    if (e.key === ' ') {
+        shootBullet();
+    }
 });
 
 window.addEventListener('keyup', (e) => {

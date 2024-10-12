dcacdeb1dc5e0535bac95dc083f31ed273aae67c
PlayerEden <159679438+PlayerEden@users.noreply.github.com>
2024-10-11 23:32:52 -0400

    Update globals.js

--- globals.js
+++ globals.js
@@ -11,8 +11,8 @@ var collectibles = [];
 
 // Player properties
 var playerSize = 20;
-var playerX = worldWidth / 2 - playerSize / 2;
-var playerY = worldHeight / 2 - playerSize / 2;
+var playerX = worldWidth / 2 - playerSize / 2; // Initial player X-coordinate (centered)
+var playerY = worldHeight / 2 - playerSize / 2; // Initial player Y-coordinate (centered)
 var playerColor = '#00FF00'; // Default color
 
 // Camera properties
